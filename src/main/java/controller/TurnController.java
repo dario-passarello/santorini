f7e596a83172beee08e28e29fac7dfd8cdc864ba
AnisRahel <61786262+AnisRahel@users.noreply.github.com>
2020-04-19 16:37:55 +0200

    Added Controller Package

---
+++ src/main/java/controller/TurnController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import model.Builder;
+import model.Game;
+import model.gods.God;
+import utils.Coordinate;
+
+import java.util.Set;
+
+public class TurnController {
+
+    private Game Model;
+    private Set<God> gods;
+    private Integer NumberOfPlayers;
+
+    public void SelectBuilder(Builder builder){
+
+    }
+
+    public void useGodPower(){
+
+    }
+
+    public void SelectCoordinate(Coordinate coordinate){
+
+    }
+
+    public TurnController(){
+
+    }
+}
