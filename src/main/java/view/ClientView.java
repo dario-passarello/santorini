c5611c4de8f4a09df6e41de40967ce3a312c76e4
Dario Passarello <dario.passarello@gmail.com>
2020-05-04 23:58:54 +0200

    Defined View Interfaces and Command Pattern Messages

---
+++ src/main/java/view/ClientView.java
@@ -0,0 +1,4 @@
+package view;
+
+public interface ClientView extends View {
+}
---
+++ src/main/java/view/View.java
@@ -0,0 +1,8 @@
+package view;
+
+import model.GameObserver;
+import model.TurnObserver;
+
+public interface View extends TurnObserver, GameObserver {
+
+}
