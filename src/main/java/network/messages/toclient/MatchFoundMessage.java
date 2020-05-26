8d93ba442655ebf1bde885f75adbaa16ce38bf0b
Dario Passarello <dario.passarello@gmail.com>
2020-05-26 15:23:21 +0200

    Implemented execute

--- src/main/java/network/messages/toclient/MatchFoundMessage.java
+++ src/main/java/network/messages/toclient/MatchFoundMessage.java
@@ -3,17 +3,18 @@ package network.messages.toclient;
 import network.messages.Message;
 import view.screens.Screen;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MatchFoundMessage implements Message<Screen> {
-    String assignedUsername;
-    List<String> players;
+    private final String assignedUsername;
+    private final List<String> players;
     public MatchFoundMessage(String assignedUsername, List<String> players){
         this.assignedUsername = assignedUsername;
         this.players = players;
     }
     @Override
     public void execute(Screen target) {
-        //TODO
+        target.receiveMatchFound(assignedUsername,new ArrayList<>(players));
     }
 }
