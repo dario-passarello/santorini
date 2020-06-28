0cda462b05959369d6c2c5bc5bf81cefc1053b50
AnisRahel <61786262+AnisRahel@users.noreply.github.com>
2020-06-28 23:27:45 +0200

    Added some Javadocs

--- src/main/java/utils/Coordinate.java
+++ src/main/java/utils/Coordinate.java
@@ -3,20 +3,36 @@ package utils;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * This class is the representation of a standard two-dimensional coordinate. It is often used to locate objects in the board
+ */
 public class Coordinate implements Serializable {
     //IMMUTABILE
     private final Integer x;
     private final Integer y;
 
+    /**
+     * The constructor of the class
+     * @param x The first coordinate (line)
+     * @param y The second coordinate (column)
+     */
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * The getter method for the first coordinate
+     * @return The value of the first coordinate
+     */
     public Integer getX(){
         return x;
     }
 
+    /**
+     * The getter method of the second coordinate
+     * @return The value of the second coordinate
+     */
     public Integer getY() {
         return y;
     }
--- src/main/java/utils/Observable.java
+++ src/main/java/utils/Observable.java
@@ -3,10 +3,29 @@ package utils;
 
 import java.util.function.Consumer;
 
+/**
+ * This interface is a custom implementation of the Observer pattern
+ * It is a parametric implementation
+ * @param <Observer>
+ */
 public interface Observable<Observer extends utils.Observer> {
 
+    /**
+     * Registers the parameter to the list of observers
+     * @param m A class that implements the Observer interface
+     */
     void registerObserver(Observer m);
+
+    /**
+     * Unregisters the parameter from the list of observers
+     * @param m A class that implements the Observer Interface
+     */
     void unregisterObserver(Observer m);
+
+    /**
+     * This method notifies a specific class of observers with the action the observable class wants them to make
+     * @param notifyAction The action to be notified
+     */
     void notifyObservers(Consumer<Observer> notifyAction);
 
 }
